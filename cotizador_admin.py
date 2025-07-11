4ca90541166330e5b13ea0a5feab1eb9893217f8
Francisco Espinosa H <50310159+PacoEspinosa@users.noreply.github.com>
2025-07-11 16:27:26 -0600

    Update cotizador_admin.py

--- cotizador_admin.py
+++ cotizador_admin.py
@@ -142,7 +142,7 @@ def identificar_ofertas():
             comision_apertura = float(request.form['comision_apertura'])
             es_credito = request.form.get('es_credito') == 'on'
 
-            text = open('config.info')
+            text = open(CONFIG_FILE)
             config = json.loads(text.read())
             cat_otros_gastos = config['catalogo_otros_gastos']
             if plazo <= 12:
